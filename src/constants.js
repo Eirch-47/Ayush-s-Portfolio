1704486469725d22a6f7cb1e9a3c430480147bdb
EiRcH <ayushgajbhiye36@gmail.com>
2024-07-18 17:10:15 +0530

    Add all my files

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Video Editing",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Ayush';
+
+export const experiences = [
+    {
+        'company': 'Mentrix Systems',
+        'role': 'Software Developer Intern',
+        'duration': 'July 2023 - Augest 2023',
+        'logo': '',
+        'points': [
+            'Developed a Desktop Application for flashing binary files onto an ECU 💻.',
+            'Acquired proficiency in industry-standard software development tools and technologies, including React⚛️.',
+            'Underwent comprehensive training in software development methodologies and tools, gaining practical knowledge 📈.',
+            'Conducted code reviews and implemented bug fixes to improve the performance and functionality of the application.⚙️.'
+        ],
+        'url': '',
+    },
+    {
+        'company': 'HDFC ERGO',
+        'role': 'Software Developer Intern',
+        'duration': 'July 2022 - November 2022',
+        'logo': 'https://upload.wikimedia.org/wikipedia/commons/3/3e/HDFC-Ergo-logo.png',
+        'points': [
+            'Worked with senior leaders to create a login page, admin panel (All things EV) project',
+            'Apache superset data engineer work, Camunda rule engine for (C.I.M.A) project.',
+            'Wi-Fi login service, recruitment process for Pune office.',
+            'Lead for Video Editing project under CTO.'
+        ],
+        'url': 'https://chase.hdfcergo.com/login',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_55lsj0h';
+export const EMAIL_JS_TEMPLATE_ID = 'template_kxoe8od';
+export const EMAIL_JS_PUBLIC_KEY = '4G_pUvSC4aTYIcqS3';
